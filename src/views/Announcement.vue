73256cbc57e73874a322ec31f4061f6f72f7f86d
吴南李 <wunanli2002@163.com>
2023-05-13 23:33:01 +0800

    完成静态组件——”放号管理“和“通知系统”
    初步完成放号管理页面和通知系统页面，修复出诊排版页面下拉框出现可选择的空值的bug

--- src/views/Announcement.vue
+++ src/views/Announcement.vue
@@ -1,65 +1,99 @@
 <template>
-	<div style="border: 1px solid #ccc">
-		<Toolbar
-				style="border-bottom: 1px solid #ccc"
-				:editor="editorRef"
-				:defaultConfig="toolbarConfig"
-				:mode="mode"
-		/>
-		<Editor
-				style="height: 500px; overflow-y: hidden;"
-				v-model="valueHtml"
-				:defaultConfig="editorConfig"
-				:mode="mode"
-				@onCreated="handleCreated"
-		/>
-	</div>
+    <div class="container">
+        <el-header>
+            <el-row>
+                <el-col :span="4">
+                    <el-icon size="30">
+                        <Notification/>
+                    </el-icon>
+                    <span style="margin-left: 5px"><strong>编辑公告</strong></span>
+                </el-col>
+                <el-col :span="4" :offset="10">
+                    <el-button type="primary" @click="">发布</el-button>
+                </el-col>
+            </el-row>
+        </el-header>
+        <el-input placeholder="请输入标题" v-model="title" style="margin-top: 10px;margin-bottom: 10px"/>
+        <div style="border: 1px solid #ccc ">
+            <Toolbar
+                    style="border-bottom: 1px solid #ccc"
+                    :editor="editorRef"
+                    :defaultConfig="toolbarConfig"
+                    :mode="mode"
+            />
+            <Editor
+                    style="height: 500px; overflow-y: hidden;"
+                    v-model="valueHtml"
+                    :defaultConfig="editorConfig"
+                    :mode="mode"
+                    @onCreated="handleCreated"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
-import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import {onBeforeUnmount, ref, shallowRef, onMounted} from 'vue'
+import {Editor, Toolbar} from '@wangeditor/editor-for-vue'
+import {Notification} from "@element-plus/icons-vue";
+
 
 export default {
-	components: { Editor, Toolbar },
-	setup() {
-		// 编辑器实例，必须用 shallowRef
-		const editorRef = shallowRef()
+    components: {Notification, Editor, Toolbar},
+    computed: {
+        initContent() {
+            return '<h1>' + this.title + '</h1>'
+        }
+    },
+    watch: {
+        title(newValue, oldValue) {
+            if (oldValue === '') {
+                this.valueHtml = this.initContent
+            } else {
+                this.valueHtml = this.valueHtml.replace(oldValue, newValue)
+            }
+        }
+    },
+    setup() {
+        const title = ref('')
+        // 编辑器实例，必须用 shallowRef
+        const editorRef = shallowRef()
 
-		// 内容 HTML
-		const valueHtml = ref('<p>hello</p>')
+        // 内容 HTML
+        const valueHtml = ref('')
 
-		// 模拟 ajax 异步获取内容
-		onMounted(() => {
-			setTimeout(() => {
-				valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
-			}, 1500)
-		})
+        // 模拟 ajax 异步获取内容
+        // onMounted(() => {
+        // 	setTimeout(() => {
+        // 		valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
+        // 	}, 1500)
+        // })
 
-		const toolbarConfig = {}
-		const editorConfig = { placeholder: '请输入内容...' }
+        const toolbarConfig = {}
+        const editorConfig = {placeholder: '请输入内容...',}
 
-		// 组件销毁时，也及时销毁编辑器
-		onBeforeUnmount(() => {
-			const editor = editorRef.value
-			if (editor == null) return
-			editor.destroy()
-		})
+        // 组件销毁时，也及时销毁编辑器
+        onBeforeUnmount(() => {
+            const editor = editorRef.value
+            if (editor == null) return
+            editor.destroy()
+        })
 
-		const handleCreated = (editor) => {
-			editorRef.value = editor // 记录 editor 实例，重要！
-		}
+        const handleCreated = (editor) => {
+            editorRef.value = editor // 记录 editor 实例，重要！
+        }
 
-		return {
-			editorRef,
-			valueHtml,
-			mode: 'default', // 或 'simple'
-			toolbarConfig,
-			editorConfig,
-			handleCreated
-		};
-	}
+        return {
+            title,
+            editorRef,
+            valueHtml,
+            mode: 'default', // 或 'simple'
+            toolbarConfig,
+            editorConfig,
+            handleCreated
+        };
+    }
 }
 </script>
--- src/views/Vacancy.vue
+++ src/views/Vacancy.vue
@@ -1,12 +1,99 @@
 <template>
     <div class="container">
-        <div class="plugins-tips">
-            放号
-        </div>
+        <el-select v-model="value" placeholder="请选择..." style="margin-bottom: 5px">
+            <el-option
+                    v-for="item in weekday"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+            />
+        </el-select>
+        <el-row>
+            <el-col :span="12">
+                <el-table :data="tableData1" style="width: 100%">
+                    <el-table-column prop="date" label="时段" width="180"/>
+                    <el-table-column label="放号数量" width="180" #default="scope">
+                        <el-input-number v-model="scope.row.amount" :min="0" :max="10"/>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+            <el-col :span="12">
+                <el-table :data="tableData2" style="width: 100%">
+                    <el-table-column prop="date" label="时段" width="180"/>
+                    <el-table-column label="放号数量" width="180" #default="scope">
+                        <el-input-number v-model="scope.row.amount" :min="0" :max="10"/>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
-<script setup lang="ts" name="vacancy">
+<script setup>
+import {ref} from 'vue'
+
+const value = ref()
+const weekday = [
+    {value: 1, label: '星期一'},
+    {value: 2, label: '星期二'},
+    {value: 3, label: '星期三'},
+    {value: 4, label: '星期四'},
+    {value: 5, label: '星期五'},
+    {value: 6, label: '星期六'},
+    {value: 7, label: '星期日'},
+]
+const tableData1 = [
+    {
+        date: '08:00-08:30',
+        amount: 10,
+    },
+    {
+        date: '08:30-09:00',
+        amount: 10,
+    },
+    {
+        date: '09:00-09:30',
+        amount: 10,
+    },
+    {
+        date: '09:30-10:00',
+        amount: 10,
+    },
+    {
+        date: '10:00-10:30',
+        amount: 10,
+    },
+    {
+        date: '10:30-11:00',
+        amount: 10,
+    },
+]
+const tableData2 = [
+    {
+        date: '14:00-14:30',
+        amount: 10,
+    },
+    {
+        date: '14:30-15:00',
+        amount: 10,
+    },
+    {
+        date: '15:00-15:30',
+        amount: 10,
+    },
+    {
+        date: '15:30-16:00',
+        amount: 10,
+    },
+    {
+        date: '16:00-16:30',
+        amount: 10,
+    },
+    {
+        date: '16:30-17:00',
+        amount: 10,
+    },
+]
 </script>
 
 <style></style>
